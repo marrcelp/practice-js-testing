74d24370bcdbb3addf8df5216305403f49930118
marrcelp <marcel.piaszczyk@gmail.com>
2025-03-27 16:59:51 +0100

    resolved exercise 1

--- 01/app.js
+++ 01/app.js
@@ -1,4 +1,5 @@
 function getProductList() {
+    
     return [
         {name: 'JavaScript: podstawy', type: 'book', count: 3, price: 67.19, discount: 0.21},
         {name: 'React: podstawy', type: 'book', count: 4, price: 79.17, discount: 0.27}
@@ -6,10 +7,12 @@ function getProductList() {
 }
 
 function getTotalPrice(productList) {
-    return productList.reduce( (product, acc) => {
-        return acc + (product.price * product.count);
-    });
+    debugger;
+    return productList.reduce( (acc, product) => {
+        const discountedPrice = product.price*(1-product.discount);
+        return acc + (discountedPrice * product.count);
+    }, 0);
 }
 
-const totalPrice = getTotalPrice( getProductList() );
+const totalPrice = getTotalPrice( getProductList() ).toFixed(2);
 console.log(totalPrice); // prawidłowa wartość: 390.42 (należy zaaokrąglić do 2 miejsc po przecinku)
