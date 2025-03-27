a51f38356dde1eed15f10cad9e1d3edf3774a60e
marrcelp <marcel.piaszczyk@gmail.com>
2025-03-27 22:16:17 +0100

    add 4 test - checkinf if random number is between min and max

--- 03/app.test.js
+++ 03/app.test.js
@@ -14,5 +14,11 @@ describe('randomNumber', () => {
         expect(() => randomNumber(3, 1)).toThrow();
     })
 
+    it('random number is between min and max props', () => {
+        const result = randomNumber(1,5);
+        expect(result).toBeGreaterThan(1);
+        expect(result).toBeLessThan(5);
+    })
+
 
 })
