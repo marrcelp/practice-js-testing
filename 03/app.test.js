54114cb0933feb091ef3bae57c517a2baa3a4825
marrcelp <marcel.piaszczyk@gmail.com>
2025-03-27 21:44:45 +0100

    add second test - when min or max is not a number

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,7 +1,14 @@
 import randomNumber from './app';
 
 describe('randomNumber', () => {
+
     it('return 1 when min = 1 and max = 1', () => {
         expect(randomNumber(1,1)).toBe(1);
     })
+
+    it('throw exception when min or max is not a number', () => {
+        expect(randomNumber(1, 'x')).toThrow();
+    })
+
+
 })
