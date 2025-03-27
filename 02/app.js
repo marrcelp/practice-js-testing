df83441846b04a64248c3cfde4d3d6600902facb
marrcelp <marcel.piaszczyk@gmail.com>
2025-03-27 17:47:51 +0100

    resolved exercise 2

--- 02/app.js
+++ 02/app.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', init);
 
+
+
 function init() {
     const clickEl = document.querySelector('.error--click');
     const enterEl = document.querySelector('.error--enter');
+    // const element = document.querySelector('.alert');
 
     setRandomPosition(clickEl);
     setRandomPosition(enterEl);
@@ -10,15 +13,33 @@ function init() {
     initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
     initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
 
+    // document.addEventListener('click', (e) => {
+    //     if (!element.contains(e.target)){
+    //         element.style.display = 'none'
+    //     }
+    // })
+    // NIE DZIALA I WTEDY NIE WYRZUCA BLEDU PO KLIKNIECIU click
+
 }
 
 function setRandomPosition(element, error = null) {
-    element.style.top = Math.random() * 600 + 'px';
-    element.style.left = Math.random() * 800 + 'px';
 
-    if(error) {
-        throw error;
+    try {
+        element.style.top = Math.random() * 600 + 'px';
+        element.style.left = Math.random() * 800 + 'px';
+
+        if(error) {
+            throw error;
+        }
+
+    } catch(err) {
+        const element = document.querySelector('.alert');
+        const alertMessage = element.querySelector('.alert__message');
+        element.style.display = 'block';
+        alertMessage.textContent = `${err.message}`
+
     }
+    
 }
 
 function initEventWithError(element, eventName, error) {
@@ -26,3 +47,4 @@ function initEventWithError(element, eventName, error) {
         setRandomPosition(this, error);
     })
 }
+
