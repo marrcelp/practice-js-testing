2861f8899833b8462d08435b0c015cf68140b32d
marrcelp <marcel.piaszczyk@gmail.com>
2025-03-31 19:54:41 +0200

    add implementation to second test

--- 04/User.js
+++ 04/User.js
@@ -1,6 +1,11 @@
 export default class User {
 
     constructor({email, password}){
+
+        if(!email.includes('@')){
+            throw new Error ('Invalid email format');
+        }
+
         this.email = email;
         this.password = password;
     }
