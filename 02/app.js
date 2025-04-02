b145bdd51ac3889308b83b525211f1bd51e90f58
marrcelp <marcel.piaszczyk@gmail.com>
2025-04-02 13:55:14 +0200

    fix try catch

--- 02/app.js
+++ 02/app.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', init);
 
-
-
 function init() {
+
     const clickEl = document.querySelector('.error--click');
     const enterEl = document.querySelector('.error--enter');
-    // const element = document.querySelector('.alert');
 
     setRandomPosition(clickEl);
     setRandomPosition(enterEl);
@@ -13,38 +11,39 @@ function init() {
     initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
     initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
 
-    // document.addEventListener('click', (e) => {
-    //     if (!element.contains(e.target)){
-    //         element.style.display = 'none'
-    //     }
-    // })
-    // NIE DZIALA I WTEDY NIE WYRZUCA BLEDU PO KLIKNIECIU click
-
 }
 
 function setRandomPosition(element, error = null) {
 
-    try {
-        element.style.top = Math.random() * 600 + 'px';
-        element.style.left = Math.random() * 800 + 'px';
+    element.style.top = Math.random() * 600 + 'px';
+    element.style.left = Math.random() * 800 + 'px';
 
-        if(error) {
-            throw error;
-        }
-
-    } catch(err) {
-        const element = document.querySelector('.alert');
-        const alertMessage = element.querySelector('.alert__message');
-        element.style.display = 'block';
-        alertMessage.textContent = `${err.message}`
-
+    if(error) {
+        throw error;
     }
-    
+    const alertElement = document.querySelector('.alert');
+
+    document.addEventListener('click', (e) => {
+        if (!alertElement.contains(e.target)){
+            element.style.display = 'none'
+        }
+    })
+    // nie dziala
 }
 
 function initEventWithError(element, eventName, error) {
     element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
+
+        try {
+            setRandomPosition(this, error);
+
+        } catch(err){
+            const element = document.querySelector('.alert');
+            const alertMessage = element.querySelector('.alert__message');
+            element.style.display = 'block';
+            alertMessage.textContent = `${err.message}`
+
+        }
     })
 }
 
